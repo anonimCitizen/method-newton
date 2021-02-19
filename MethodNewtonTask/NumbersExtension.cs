6546ce10bc56664be6b613bd603ef2c39e1f9ef8
anonimCitizen <68064734+anonimCitizen@users.noreply.github.com>
2021-02-20 01:47:05 +0300

    Add files via upload

---
+++ MethodNewtonTask/NumbersExtension.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace MethodNewtonTask
+{
+    public static class NumbersExtension
+    {
+        /// <summary>
+        /// Initializes static members of the <see cref="NumbersExtension"/> class.
+        /// </summary>
+        public static readonly AppSettings AppSettings = new AppSettings { Epsilon = double.Epsilon };
+
+        /// <summary>
+        /// Find n-th root of number with the given accuracy.
+        /// </summary>
+        /// <param name="number">Source number.</param>
+        /// <param name="degree">Root degree.</param>
+        /// <param name="accuracy">Accuracy value.</param>
+        /// <returns> n-th root of number.</returns>
+        /// <exception cref="ArgumentException">
+        /// Thrown when number is negative and n degree is even.
+        /// -or- 
+        /// degree is less than zero
+        /// -or-
+        /// number is NaN, double.NegativeInfinity, double.PositiveInfinity.
+        /// </exception>
+        /// <exception cref="ArgumentOutOfRangeException">
+        /// Thrown when accuracy is less than zero.
+        /// -or- 
+        /// accuracy is more than `Epsilon`.
+        /// </exception>
+        public static double FindNthRoot(double number, int degree, double accuracy)
+        {
+            double x = number;
+            double xPrev;
+            if (degree % 2 == 0 && number < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (degree < 0)
+            {
+                throw new ArgumentException();
+            }
+
+            if (double.IsNegativeInfinity(number) || double.IsPositiveInfinity(number) || double.IsNaN(number))
+            {
+                throw new ArgumentException();  
+            }
+
+            if (accuracy < 0 || accuracy > AppSettings.Epsilon)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+
+
+            do
+            {
+                xPrev = x;
+                x = ((double)1 / degree) * ((double)(degree - 1) * xPrev + (double)number / (double)Math.Pow(xPrev, degree - 1));
+            } while (Math.Abs(x - xPrev) >= AppSettings.Epsilon); 
+            return x;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# method-newton
+## Task description ##
+
+Develop a [FindNthRoot](MethodNewtonTask/NumbersExtension.cs#L30) method that implements an algorithm of the [Newton method](https://en.wikipedia.org/wiki/Nth_root_algorithm) to calculate approximation $`\sqrt[n]{a}`$, where `n > 0`- an integer, `a`- a real number (`System.Double`), `accuracy`- a given accuracy, `accuracy ⩽ epsilon, 0 < epsilon < 1, epsilon`- a real number (`System.Double`). The value of the `epsilon`- constant is set in the [configuration file](MethodNewtonTask.Tests/AppSettings.json).
+
+As the found value $`\sqrt[n]{a}`$ use such a value of $`x_k`$ at which $`|x_k-x_{k-1}|<ε`$, $`x_{k} = 1/n((n-1)x_{k-1}+a/x_{k-1}^{n-1}), k=1,2,..., x_0=a`$.   
+
+*Topics -  loops with an unknown number of iterations, struct System.Double, equality for System.Double, iterative algorithms.*
